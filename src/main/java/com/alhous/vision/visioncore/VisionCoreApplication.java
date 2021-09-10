d0f35b0586e6f51496c9bc88eb0565d21a05cd2e
silahi <silahi.alihoussene@gmail.com>
2021-09-10 15:16:42 +0000

    Initialisation du module vision-core

---
+++ src/main/java/com/alhous/vision/visioncore/VisionCoreApplication.java
@@ -0,0 +1,13 @@
+package com.alhous.vision.visioncore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisionCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisionCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alhous/vision/visioncore/VisionCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alhous.vision.visioncore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisionCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
