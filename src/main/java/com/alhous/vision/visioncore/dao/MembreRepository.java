50f5aecc1a12a82abf7bc9d2981270bf4c3d1372
silahi <silahi.alihoussene@gmail.com>
2021-09-11 12:27:40 +0000

    Changement de l'interface JpaRepository par CrudRepository

--- src/main/java/com/alhous/vision/visioncore/dao/MembreRepository.java
+++ src/main/java/com/alhous/vision/visioncore/dao/MembreRepository.java
@@ -2,8 +2,8 @@ package com.alhous.vision.visioncore.dao;
 
 import com.alhous.vision.visioncore.entite.Membre;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface MembreRepository extends JpaRepository<Membre, Long> {
+public interface MembreRepository extends CrudRepository<Membre, Long> {
     
 }
--- src/main/java/com/alhous/vision/visioncore/dao/PhotoRepository.java
+++ src/main/java/com/alhous/vision/visioncore/dao/PhotoRepository.java
@@ -2,8 +2,8 @@ package com.alhous.vision.visioncore.dao;
 
 import com.alhous.vision.visioncore.entite.Photo;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
+public interface PhotoRepository extends CrudRepository<Photo, Long> {
     
 }
